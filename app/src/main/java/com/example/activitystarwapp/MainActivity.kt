3924d84fed23cc4c8200b4e97082a47ee5b7bc27
rfvieira <rfgvieira.01@gmail.com>
2022-02-03 18:58:12 -0300

    feat: implementaçaõ de tabs na busca geral e criação da tela de busca por id

--- app/src/main/java/com/example/activitystarwapp/MainActivity.kt
+++ app/src/main/java/com/example/activitystarwapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.activitystarwapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,14 +19,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var tabLayout = binding.tlMain
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.person).setId(0))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.planet).setId(1))
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.nave).setId(2))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.id){
+                    0 -> { binding.tvSubtitle.setText(R.string.personagens)
+                    getData(0)}
+                    1 -> { binding.tvSubtitle.setText(R.string.planetas)
+                    getData(1)}
+                    2 -> { binding.tvSubtitle.setText(R.string.espaconaves)
+                    getData(2)}
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 
-    override fun onResume() {
-        super.onResume()
-        getData()
-    }
-
-    private fun getData(){
+    private fun getData(flag : Int){
         val retroFit = RetroFit.getRetrofitInstance("https://swapi.dev/api/")
         val endpoint = retroFit.create(Endpoint ::class.java)
         val callback = endpoint.getPeople()
