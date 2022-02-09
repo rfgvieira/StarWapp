8a810fae97a2a2dce48e2a334a8901df4999f043
rfgvieira <rfgvieira.01@gmail.com>
2022-02-09 17:43:49 -0300

    feat: implementação do novo layout da splash, menu e inicio da implementação em todos personagens

--- app/src/main/java/com/example/activitystarwapp/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/activitystarwapp/presentation/activity/MainActivity.kt
@@ -1,26 +1,109 @@
 package com.example.activitystarwapp.presentation.activity
 
+import android.app.Dialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.Window
+import com.example.activitystarwapp.R
 import com.example.activitystarwapp.databinding.ActivityMenuBinding
+import com.example.activitystarwapp.databinding.PopupBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMenuBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnBuscaid.setOnClickListener {
-            val intent = Intent(this, BuscaActivity::class.java)
-            startActivity(intent)
+        binding.btnNave.setOnClickListener {
+            showPopUp(0)
         }
 
-        binding.btnBuscatudo.setOnClickListener {
-            val intent = Intent(this, TodosActivity::class.java)
-            startActivity(intent)
+        binding.btnPlaneta.setOnClickListener {
+            showPopUp(1)
+        }
+
+        binding.btnPersonagem.setOnClickListener {
+            showPopUp(2)
+
+        }
+
+        binding.btnAleatorio.setOnClickListener {
+            showPopUp(3)
+        }
+
+
+
+    }
+
+    private fun showPopUp(tipo: Int) {
+
+        val bindingPopupBinding = PopupBinding.inflate(layoutInflater)
+
+        bindingPopupBinding.btnPopuptudo.setOnClickListener {
+            redirectActivity(1,0)
+        }
+
+        bindingPopupBinding.btnPopupid.setOnClickListener {
+            redirectActivity(1,1)
+        }
+
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setContentView(bindingPopupBinding.root)
+
+
+        val window = dialog.window
+        window?.let {
+            it.attributes.gravity = Gravity.BOTTOM
+            it.attributes.width = this.window.attributes.width
+            it.setBackgroundDrawable(getDrawable(R.drawable.roundedbtn))}
+
+        dialog.show()
+
+
+
+    }
+
+
+    private fun redirectActivity(tipo : Int, modo : Int) {
+        when(tipo){
+            0 -> {
+                if(modo == 0) {
+                    val intent = Intent(this, TodosActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, BuscaActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            1 -> {
+                if(modo == 0) {
+                    val intent = Intent(this, TodosActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, BuscaActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+
+            2 -> {
+                if(modo == 0) {
+                    val intent = Intent(this, TodosActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, BuscaActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/activitystarwapp/presentation/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.activitystarwapp.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.activitystarwapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
